84650ae2cef50cd5094bbb3357fd94896657fd41
Fatimatou-Ezzahrae <oudghirifaty1@gmail.com>
2019-09-17 17:14:12 +0100

    first commit

---
+++ app/src/main/java/com/example/versionalpha/Splash.java
@@ -0,0 +1,39 @@
+package com.example.versionalpha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    private ImageView iv;
+    private ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        iv = (ImageView) findViewById(R.id.iv);
+        logo = (ImageView) findViewById(R.id.logo);
+        Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.transition);
+        iv.startAnimation(myAnim);
+        logo.startAnimation(myAnim);
+        final Intent i = new Intent(this, LoginActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
